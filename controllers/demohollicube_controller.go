28add5164a0c44ecba58a75d4a2caf29d957bf1a
liyongxian <liyongxian@hollysys.com>
2021-02-19 18:03:57 +0800

    aaa

--- controllers/demohollicube_controller.go
+++ controllers/demohollicube_controller.go
@@ -25,8 +25,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	gpaasv1alpha1 "demo-operator/api/v1alpha1"
 )
@@ -114,8 +114,8 @@ func (r *DemoHollicubeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 			},
 		},
 	}
-	
-	// set Ref 
+
+	// set Ref
 	log.Info("set reference")
 	if err := controllerutil.SetControllerReference(demoHollicube, deployment, r.Scheme); err != nil {
 		log.Error(err, "SetControllerReference error")
